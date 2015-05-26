1796f8e268e4b26df53fd9916b41d319a69f7e2f
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-05-26 19:55:19 -0300

    Removed hybrid code
    Started full cpp class code

---
+++ schedproc.cpp
@@ -0,0 +1,58 @@
+#include "c_schedproc.h"
+
+
+// TODO: Check struct call to C typedef
+struct schedproc Schedproc::toStruct()
+{
+	struct schedproc retStruct;
+	retStruct.endpoint = endpoint;
+	retStruct.parent = parent;
+	retStruct.flags = flags;
+	retStruct.max_priority = max_priority;
+	retStruct.priority = priority;
+	retStruct.base_time_slice = base_time_slice;
+	retStruct.time_slice = time_slice;
+	retStruct.cpu = cpu;
+	retStruct.cpu_mask[BITMAP_CHUNKS] = cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)];
+	retStruct.burst_history[BURST_HISTORY_LENGTH] = burst_history[BURST_HISTORY_LENGTH];
+	retStruct.burst_hist_cnt = burst_hist_cnt;
+	return retStruct;
+}
+
+
+void Schedproc::setValues (struct schedproc argStruct)
+{
+	endpoint = argStruct.endpoint;
+	parent = argStruct.parent;
+	flags = argStruct.flags;
+	max_priority = argStruct.max_priority;
+	priority = argStruct.priority;
+	base_time_slice = argStruct.base_time_slice;
+	time_slice = argStruct.time_slice;
+	cpu = argStruct.cpu;
+	cpu_mask[BITMAP_CHUNKS = argStruct.cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)];
+	burst_history[BURST_HISTORY_LENGTH] = argStruct.burst_history[BURST_HISTORY_LENGTH];
+	burst_hist_cnt = argStruct.burst_hist_cnt;
+}
+
+int Schedproc::do_stop_scheduling(message *m_ptr)
+{
+	int proc_nr_n;
+
+	/* check who can send you requests */
+	if (!accept_message(m_ptr))
+		return EPERM;
+
+	if (sched_isokendpt(m_ptr->SCHEDULING_ENDPOINT, &proc_nr_n) != OK) {
+		printf("SCHED: WARNING: got an invalid endpoint in OOQ msg "
+		"%ld\n", m_ptr->SCHEDULING_ENDPOINT);
+		return EBADEPT;
+	}
+
+	this = listSched[proc_nr_n];
+#ifdef CONFIG_SMP
+	cpu_proc[this->cpu]--;
+#endif
+	this->flags = 0; /*&= ~IN_USE;*/
+	return OK;
+}
--- schedproc.h
+++ schedproc.h
@@ -1,42 +1,85 @@
-/* This table has one slot per process.  It contains scheduling information
- * for each process.
- */
+// // TODO REMOVE UNNECESSARY STMTS
+
+// /* This table has one slot per process.  It contains scheduling information
+//  * for each process.
+//  */
+// #include <limits.h>
+
+// #include <minix/bitmap.h>
+
+// /* EXTERN should be extern except in main.c, where we want to keep the struct */
+// #ifdef _MAIN
+// #undef EXTERN
+// #define EXTERN
+// #endif
+
+// #ifndef CONFIG_SMP
+// #define CONFIG_MAX_CPUS 1
+// #endif
+
+// /**
+//  * We might later want to add more information to this table, such as the
+//  * process owner, process group or cpumask.
+//  */
+// #define BURST_HISTORY_LENGTH 10
+// EXTERN struct schedproc {
+// //struct schedproc {
+// 	endpoint_t endpoint;	/* process endpoint id */
+// 	endpoint_t parent;	/* parent endpoint id */
+// 	unsigned flags;		/* flag bits */
+
+// 	/* User space scheduling */
+// 	unsigned max_priority;	/* this process' highest allowed priority */
+// 	unsigned priority;		/* the process' current priority */
+// 	unsigned base_time_slice;
+// 	unsigned time_slice;		/* this process's time slice */
+// 	unsigned cpu;
+// 	bitchunk_t cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)];
+
+// 	unsigned burst_history[BURST_HISTORY_LENGTH];
+// 	unsigned burst_hist_cnt;
+// } schedproc[NR_PROCS];
+
+// /* Flag values */
+// #define IN_USE		0x00001	/* set when 'schedproc' slot in use */
+
+// TODO: CHECK THESE INCLUDES
+
+#ifndef SCHD_H
+#define SCHD_H
+#include <list>
 #include <limits.h>
-
 #include <minix/bitmap.h>
+#include "sched.h"
 
-/* EXTERN should be extern except in main.c, where we want to keep the struct */
-#ifdef _MAIN
-#undef EXTERN
-#define EXTERN
-#endif
 
 #ifndef CONFIG_SMP
 #define CONFIG_MAX_CPUS 1
 #endif
 
-/**
- * We might later want to add more information to this table, such as the
- * process owner, process group or cpumask.
- */
 #define BURST_HISTORY_LENGTH 10
-EXTERN struct schedproc {
-//struct schedproc {
-	endpoint_t endpoint;	/* process endpoint id */
-	endpoint_t parent;	/* parent endpoint id */
-	unsigned flags;		/* flag bits */
 
-	/* User space scheduling */
-	unsigned max_priority;	/* this process' highest allowed priority */
-	unsigned priority;		/* the process' current priority */
-	unsigned base_time_slice;
-	unsigned time_slice;		/* this process's time slice */
-	unsigned cpu;
-	bitchunk_t cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)];
+class Schedproc
+{
+	private:
+	   	endpoint_t endpoint;	/* process endpoint id */
+		endpoint_t parent;		/* parent endpoint id */
+		unsigned flags;			/* flag bits */
 
-	unsigned burst_history[BURST_HISTORY_LENGTH];
-	unsigned burst_hist_cnt;
-} schedproc[NR_PROCS];
+		/* User space scheduling */
+		unsigned max_priority;		/* this process' highest allowed priority */
+		unsigned priority;			/* the process' current priority */
+		unsigned base_time_slice;
+		unsigned time_slice;		/* this process's time slice */
+		unsigned cpu;
+		bitchunk_t cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)];
+		unsigned burst_history[BURST_HISTORY_LENGTH];
+		unsigned burst_hist_cnt;
+  	public:
+	    void setValues(struct schedproc argStruct);
+	    struct schedproc toStruct ();
+ };
 
-/* Flag values */
-#define IN_USE		0x00001	/* set when 'schedproc' slot in use */
+ extern std::list<Schedproc> listSched(NR_PROCS)
+
+ #endif
