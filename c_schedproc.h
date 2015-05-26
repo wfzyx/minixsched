a4d97017d20f07cc03bf1edff2b3ea5b8e1cee4a
Beletti <rhiguita@gmail.com>
2015-05-26 15:36:57 -0700

    Update c_schedproc.h
    Fixed issue with BURST_HISTORY_LENGTH (line 26) and BITMAP_CHUNKS (line 25).

--- c_schedproc.h
+++ c_schedproc.h
@@ -3,6 +3,12 @@
 #include "sched.h"
 //#include "schedproc.h"
 
+#ifndef CONFIG_SMP
+#define CONFIG_MAX_CPUS 1
+#endif
+
+#define BURST_HISTORY_LENGTH 10
+
 // somente define a classe se o include for chamado no C++
 #ifdef __cplusplus
  class Schedproc2 {
@@ -16,8 +22,8 @@
 	unsigned base_time_slice;
 	unsigned time_slice;		/* this process's time slice */
 	unsigned cpu;
-	//bitchunk_t cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)];
-	//unsigned burst_history[BURST_HISTORY_LENGTH];
+	bitchunk_t cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)];
+	unsigned burst_history[BURST_HISTORY_LENGTH];
 	unsigned burst_hist_cnt;
   //public:
   //  Schedproc();
@@ -54,7 +60,7 @@ struct schedproc Schedproc::toStruct(){
 	retStruct.time_slice = time_slice;
 	retStruct.cpu = cpu;
 	//retStruct.cpu_mask[BITMAP_CHUNKS] = cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)];
-	//retStruct.burst_history[BURST_HISTORY_LENGTH] = burst_history[BURST_HISTORY_LENGTH];
+	retStruct.burst_history[BURST_HISTORY_LENGTH] = burst_history[BURST_HISTORY_LENGTH];
 	retStruct.burst_hist_cnt = burst_hist_cnt;
 	return retStruct;
 }
@@ -71,7 +77,7 @@ Schedproc::Schedproc (struct schedproc argStruct) {
 	time_slice = argStruct.time_slice;
 	cpu = argStruct.cpu;
 	//cpu_mask[BITMAP_CHUNKS = argStruct.cpu_mask[BITMAP_CHUNKS(CONFIG_MAX_CPUS)];
-	//burst_history[BURST_HISTORY_LENGTH] = argStruct.burst_history[BURST_HISTORY_LENGTH];
+	burst_history[BURST_HISTORY_LENGTH] = argStruct.burst_history[BURST_HISTORY_LENGTH];
 	burst_hist_cnt = argStruct.burst_hist_cnt;
 
 }
