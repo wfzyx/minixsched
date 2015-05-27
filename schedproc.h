26c8126e60e4058c87cd7d6e8e025fdb968f8241
Beletti <rhiguita@gmail.com>
2015-05-27 12:43:29 -0700

    Update schedproc.h

--- schedproc.h
+++ schedproc.h
@@ -78,6 +78,7 @@ class Schedproc
 	    // void setValues(struct schedproc argStruct);
 	    // struct schedproc toStruct ();
 	    // Schedproc(Schedproc &cSrc);
+	    int burst_smooth(unsigned burst);
 	    int do_stop_scheduling(message *m_ptr);
 	    int sched_isokendpt(int endpoint, int *proc);
 	    int sched_isemtyendpt(int endpoint, int *proc);
