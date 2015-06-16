48178f75108684836357a071acec9a8a62fc69f7
Beletti <rhiguita@gmail.com>
2015-06-15 17:01:54 -0700

    Update schedproc.h

--- schedproc.h
+++ schedproc.h
@@ -35,7 +35,7 @@ class Schedproc
 	    	int do_stop_scheduling();
 	    	int do_nice(unsigned new_q);
 	    	int do_noquantum(unsigned ipc);
-	    	int do_start_scheduling(int m_type, endpoint_t endp, endpoint_t parent, unsigned maxprio, unsigned quantum);
+	    	int do_start_scheduling(int mtp, endpoint_t endp, endpoint_t parent, unsigned maxprio, unsigned quantum);
 };
 Schedproc schedproc[NR_PROCS];
 #else
